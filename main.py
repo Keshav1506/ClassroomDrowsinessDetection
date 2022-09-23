374e3ab8c513d042ef8095e695575ef13fd6a9f9
Arvind Krishna <49233881+Arvind-krishna@users.noreply.github.com>
2022-09-24 03:46:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,66 @@
 from scipy.spatial import distance
+from PIL import Image
+import numpy
+from streamlit_webrtc import (
+    RTCConfiguration,
+    WebRtcMode,
+    WebRtcStreamerContext,
+    webrtc_streamer,
+)
 import imutils
 from imutils import face_utils
 import streamlit as st
+from av import VideoFrame as vf
 
 import dlib
 import cv2
 
+thresh = 0.25
+ear=0.0
+flag=0
+frame_check = 20
+detect = dlib.get_frontal_face_detector()
+predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["mouth"]
 
 
+
+def framefn(frame):
+    global ear,flag
+    img = frame.to_image()
+    img.save("test.jpg")
+    img=cv2.imread("test.jpg")
+
+    frame = imutils.resize(img, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    subjects = detect(gray, 0)
+    for subject in subjects:
+        shape = predict(gray, subject)
+        shape = face_utils.shape_to_np(shape)  # converting to NumPy Array
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        mouth = shape[mStart:mEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        ears = (leftEAR + rightEAR) / 2.0
+        ear= ears
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        mHull = cv2.convexHull(mouth)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [mHull], -1, (0, 255, 0), 1)
+
+        if ear <= 0.5:
+            flag += 1
+
+
+    Image.fromarray(frame).save("test1.jpg")
+    img = Image.open("test1.jpg")
+    return vf.from_image(img)
 
 def eye_aspect_ratio(eye):
     A = distance.euclidean(eye[1], eye[5])
@@ -32,67 +85,24 @@ st.image("image.jfif")
 
 
 
-placeholder = st.empty()
 status = st.empty()
+infnc=st.empty()
+framenumber= st.empty()
 
 
 frame_check = 10
-detect = dlib.get_frontal_face_detector()
-predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")  # Dat file is the crux of the code
 
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
-(MoStart, MoEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["mouth"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+webrtc_streamer(key="opencv-filter",mode=WebRtcMode.SENDRECV,video_frame_callback=framefn,async_processing=True,media_stream_constraints={"video": True, "audio": False})
 
-
-if st.button("Start proctoring"):
-
-    cap = cv2.VideoCapture(0)
+infnc.text(ear)
+framenumber.text(flag)
+if flag >= frame_check:
+    status.markdown("<h1 style='text-align: center; color: red;'>Drowsy!</h1>", unsafe_allow_html=True)
 
 
+else:
+    status.markdown("<h1 style='text-align: center; color: green;'>Active</h1>", unsafe_allow_html=True)
     flag = 0
-    while True:
-        ret, frame = cap.read()
-        frame = imutils.resize(frame, width=450)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        subjects = detect(gray, 0)
-        for subject in subjects:
-
-            shape = predict(gray, subject)
-            shape = face_utils.shape_to_np(shape)  # converting to NumPy Array
-            mouth = shape[MoStart:MoEnd]
-            leftEye = shape[lStart:lEnd]
-            rightEye = shape[rStart:rEnd]
-            leftEAR = eye_aspect_ratio(leftEye)
-            rightEAR = eye_aspect_ratio(rightEye)
-            mouthEAR = mouth_aspect_ratio(mouth)
-            thresh=0.25 #thresh
-            ear = (leftEAR + rightEAR ) / 2.0
-            leftEyeHull = cv2.convexHull(leftEye)
-            rightEyeHull = cv2.convexHull(rightEye)
-            mouthHull = cv2.convexHull(mouth)
-            cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-            cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-            cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
-            placeholder.image(frame)
-
-
-
-
-            if ear < thresh:
-                flag += 1
-                if flag >= frame_check:
-                    status.markdown("<h1 style='text-align: center; color: red;'>Drowsy!</h1>",unsafe_allow_html=True)
-
-
-            else:
-                status.markdown("<h1 style='text-align: center; color: green;'>Active</h1>",unsafe_allow_html=True)
-                flag = 0
-
-if st.button("Stop proctoring"):
-    st.stop()
-
-
 
 
 
